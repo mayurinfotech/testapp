b8822303e01270b58a85376cde0d4a87e97a85c9
mayurinfotech <mayur.ranshinge@nimapinfotech.com>
2025-10-01 10:43:46 +0530

    first commit

---
+++ lib/features/activity_screen/presentation/screen/view_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class ViewScreen extends StatelessWidget {
+  const ViewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+    );
+  }
+}
